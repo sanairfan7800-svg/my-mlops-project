4668d50096438e0fe139f25f01225af9477ddbc0
sanairfan7800-svg <sanairfan7800@gmail.com>
2026-04-15 00:03:45 +0500

    Added Flask web server code

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("MLOps Project 1: Containerized App is Running!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>MLOps Project 1: Containerized App is Running!</h1>"
+
+if __name__ == "__main__":
+    # Host 0.0.0.0 zaroori hai Docker ke liye
+    app.run(host='0.0.0.0', port=5000)print("MLOps Project 1: Containerized App is Running!")
