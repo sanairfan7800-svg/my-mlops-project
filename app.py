6217cabb63a4cc873896a3bcfa00d9263d9c97db
sanairfan7800-svg <sanairfan7800@gmail.com>
2026-04-21 22:31:21 +0500

    Fixed syntax in app.py

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ def hello():
     return "<h1>MLOps Project 1: Containerized App is Running!</h1>"
 
 if __name__ == "__main__":
-    # Host 0.0.0.0 zaroori hai Docker ke liye
-    app.run(host='0.0.0.0', port=5000)print("MLOps Project 1: Containerized App is Running!")
+    app.run(host='0.0.0.0', port=5000)
